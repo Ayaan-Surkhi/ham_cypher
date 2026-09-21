8ca3a9a4b673d86c0658ca8753804ba1d64cd74c
Ayaan Surkhi <ayaansurkhi@gmail.com>
2026-09-21 21:07:02 +0530

    page links added

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const NUM_RINGS = 5;
 const TARGET_HASH = "582969a27a84f2b65970db6cb1907706e6518405ced1876352f6627d794b430e";
 const STATIONS = [
-  { label: "Question 1", url: "#" },
+  { label: "Question 1", url: "https://cipherq1.netlify.app" },
   { label: "Question 2", url: "#" },
-  { label: "Question 3", url: "#" },
+  { label: "Question 3", url: "https://mafia-wiretap.netlify.app/" },
   { label: "Question 4", url: "#" },
   { label: "Question 5", url: "/final/final.html" }
 ];
